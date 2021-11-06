f144c2bffb256e0613cab39d740eff53ba34ea02
Macawls <joshua.macauley@tutanota.com>
2021-11-06 12:39:17 +0100

    eksdeee lol

--- scripts/updateconfig.sh
+++ scripts/updateconfig.sh
@@ -6,20 +6,22 @@
 white="\033[1;37m"
 blue="\033[1;34m"
 black="\033[1;40;30m"
-reset="\033[0m"
 yellow="\033[1;33m"
 green="\033[1;32m"
 red="\033[1;31m"
+reset="\033[0m"
 
 
-#fuq me
-dots="/usr/bin/git --git-dir=$HOME/.cfg/.git/ --work-tree=$HOME" #bare repo
+# :)
+dots="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME" #bare repo
+
 remoteurl="$dots remote show origin | awk '/Push/ {print \$3}'" #origin url for repo
+headbranch="$dots branch | awk '{print \$2}'"
+
+branch=$(eval $headbranch)
 remote=$(eval $remoteurl)
 
-clear 
-#https://github.com/azur1s/octofetch
-
+clear
 #output
  cat << "EOF"
  	 :oydmNNmdyo:         
@@ -36,8 +38,8 @@ clear
 EOF
 
 
-echo -e "\n${yellow}Pushing to  ${green}$remote${green}"
-echo -e "\n${blue}Enter commit message to push to github lol${blue}${white} \n"
+echo -e "\n${yellow}Pushing to  ${green}$remote${green}\n${red}Branch  ${red}$branch"
+echo -e "\n${blue}Enter commit message to push all changes to github lol${blue}${white} \n"
 echo -n "> "
 read msg
 while true; do
@@ -45,13 +47,12 @@ while true; do
     echo -n "> "
     read yn
     case $yn in
-        [Yy]* ) $dots commit -a -m "$input"; exit;;
+        [Yy]* ) $dots commit -a -m "$msg"; exit;;
         [Nn]* ) exit;;
         * ) echo -e "${red}Answer YES or NO u sexy mofo${red}";;
     esac
 done
 
-#$dots commit -a -m "$msg"
-#$dots push
+$dots push
 
 
