2cd9d1c92d428c9fd78967dda904a15b5d4c87e8
Macawls <joshua.macauley@tutanota.com>
2021-11-11 00:50:51 +0100

    w w w w w w w w w

--- scripts/mouse.sh
+++ scripts/mouse.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-xinput set-prop 11 "Coordinate Transformation Matrix" 0.55000, 0.000000, 0.000000, 0.000000, 0.55000, 0.000000, 0.000000, 0.000000, 1.000000
+xinput set-prop "pointer:Logitech MX Master 2S" "Coordinate Transformation Matrix" 0.55000, 0.000000, 0.000000, 0.000000, 0.55000, 0.000000, 0.000000, 0.000000, 1.000000
