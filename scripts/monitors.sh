c701c44334e9b3f5b0c0bafbd8dd51fa3ca39206
Macawls <joshua.macauley@tutanota.com>
2022-02-01 22:20:10 +0200

    xd

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -4,6 +4,8 @@ primary="DP-0"
 secondary="DVI-D-0"
 setting="--mode 1920x1080 --rate 144.00"
 
-xrandr --output $primary $setting --primary 
+xrandr --output $primary $setting --primary
+bspc monitor $primary -d 1 2 3 4
 xrandr --output $secondary $setting --right-of $primary
+bspc monitor $secondary -d 5 6 7 8
 
